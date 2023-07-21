ed8deb46dce1c27250b78118a3eabe11c0c5bb7d
Michael Xu <michael.xu1816@gmail.com>
2023-07-21 15:18:06 -0400

    Fixed visualization styling

--- touchsim/texture.py
+++ touchsim/texture.py
@@ -20,19 +20,22 @@ class DisplacementMap:
         self.max_height = max_height
         self.bitmap = self._get_bitmap(self.filename, self.size, self.max_height)
         self.interpolator = RegularGridInterpolator([np.arange(i) for i in self.bitmap.shape], self.bitmap,
-                                                    bounds_error=False, fill_value=None, method="linear")
+                                                    bounds_error=False, fill_value=None, method="cubic")
 
     def height_at(self, x: float, y: float) -> float:
         return self.interpolator((x, y))
 
     def visualize(self, coords: tuple[float, float] = None,
                   show_original_points=False, surface_type: Literal["square", "triangular", "mesh"] = "triangular",
-                  full_screen=False, resolution=100) -> None:
-        fig = plt.figure(figsize=(10, 10))
+                  full_screen=False, hide_axis=True, resolution=100) -> None:
+        fig = plt.figure(figsize=(8, 8))
         plt.subplots_adjust(bottom=0, left=0, right=1, top=1)
         ax = fig.add_subplot(projection="3d", computed_zorder=False)  # WHAT THE HECK MATPLOTLIB!!!!
-        ax.set_zlim(0, 255)
-        ax.axis("off")
+        ax.set_zmargin((255 - self.max_height) / self.max_height / 2)
+
+        if hide_axis:
+            ax.dist = 8
+            ax.axis("off")
 
         if show_original_points:
             x, y = np.meshgrid(np.arange(self.bitmap.shape[0]), np.arange(self.bitmap.shape[1]))
@@ -50,7 +53,7 @@ class DisplacementMap:
                             cmap="viridis", edgecolor="none")
 
         if coords is not None:
-            ax.scatter(*coords, self.height_at(*coords), c="m", marker=7, s=100, zorder=3)
+            ax.scatter(*coords, self.height_at(*coords), c="fuchsia", marker=7, s=100, zorder=3)
 
         if full_screen:
             plt.get_current_fig_manager().full_screen_toggle()
@@ -77,5 +80,5 @@ class DisplacementMap:
 
 
 if __name__ == "__main__":
-    texture = DisplacementMap("../textures/rock.jpg", size=(10, 10))
-    texture.visualize((3, 4))
+    texture = DisplacementMap("../textures/rock.jpg")
+    texture.visualize((50, 150))
