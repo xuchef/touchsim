e6047660c23d6b580bc715102fef58697dddd264
Michael Xu <michael.xu1816@gmail.com>
2023-08-21 22:24:34 -0600

    Decouple constants in helper folder from touchsim

--- experiments/scripts/helper/constants.py
+++ experiments/scripts/helper/constants.py
@@ -1,7 +1,6 @@
 from os.path import join
-from touchsim.constants import affclasses
 
 TEXTURE_SETS_DIR = join("experiments", "texture_sets")
 DATASETS_DIR = join("experiments", "datasets")
 MODELS_DIR = join("experiments", "models")
-AFF_CHOICES = affclasses + ["all"]
+AFF_CHOICES = ["SA1", "RA", "PC", "all"]
